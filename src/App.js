72747411d9d31cc15838a4154c248c0c676b225e
steve911-prog <galja2908@gmail.com>
2022-09-14 21:15:29 +0200

    adding bootstrap

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return <div className="App">
+    <button className="btn btn-primary">Hello world</button>
+  </div>;
 }
 
 export default App;
